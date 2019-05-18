77c30750209cb08a1a00b1046a9ef298b8cacace
David Fang <Fang915685845@gmail.com>
2019-05-18 15:01:46 +0800

    asdfghjkl;

---
+++ stu201831980117/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="stu201831980117.MvcApplication" Language="C#" %>
---
+++ stu201831980117/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(stu201831980117.Startup))]
+namespace stu201831980117
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
